900a025ca004c1830784e36c9ec8c6d677a2fde3
Salih <mustafahakan9215@gmail.com>
2024-11-02 16:23:12 +0300

    added RestaurantTable entity

---
+++ src/main/java/com/webapp/restaurant_booking/controller/ReservationApiController.java
@@ -0,0 +1,51 @@
+package com.webapp.restaurant_booking.controller;
+
+import com.webapp.restaurant_booking.models.Reservation;
+import com.webapp.restaurant_booking.repo.ReservationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequestMapping("/api/reservations")
+@RestController
+public class ReservationApiController {
+    @Autowired
+    private ReservationRepo reservationRepo;
+
+    @PostMapping
+    public Reservation createReservation(@RequestBody Reservation reservation) {
+        return reservationRepo.save(reservation);
+    }
+
+    @GetMapping
+    public List<Reservation> getAllReservations() {
+        return reservationRepo.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Reservation getSingleReservation(@PathVariable("id") long id){
+        return reservationRepo.findById(id).get();
+    }
+
+    @PutMapping("/{id}")
+    public Reservation updateReservation(@PathVariable Long id, @RequestBody Reservation reservationDetails) {
+        Reservation reservation = reservationRepo.findById(id).get();
+
+        reservation.setReservationTime(reservationDetails.getReservationTime());
+        reservation.setRestaurant(reservationDetails.getRestaurant());
+        reservation.setUser(reservationDetails.getUser());
+
+        return reservationRepo.save(reservation);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public boolean removeRestaurant(@PathVariable("id") long id){
+        if(!reservationRepo.findById(id).equals(Optional.empty())){
+            reservationRepo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
