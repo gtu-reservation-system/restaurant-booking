eccecb140dcf9c976914de9148933caef1c7d3d7
Salih <mustafahakan9215@gmail.com>
2024-11-06 23:43:20 +0300

    added service layer for restaurant api controller

--- src/main/java/com/webapp/restaurant_booking/controller/RestaurantApiController.java
+++ src/main/java/com/webapp/restaurant_booking/controller/RestaurantApiController.java
@@ -1,9 +1,7 @@
 package com.webapp.restaurant_booking.controller;
 
 import com.webapp.restaurant_booking.models.Restaurant;
-import com.webapp.restaurant_booking.models.RestaurantTable;
-import com.webapp.restaurant_booking.repo.RestaurantRepo;
-import com.webapp.restaurant_booking.repo.TableRepo;
+import com.webapp.restaurant_booking.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,65 +12,30 @@ import java.util.*;
 public class RestaurantApiController {
 
     @Autowired
-    private RestaurantRepo restaurantRepo;
-
-    @Autowired
-    private TableRepo tableRepo;
+    private RestaurantService restaurantService;
 
     @GetMapping()
-    public List<Restaurant> getAllRestaurants(){
-        return restaurantRepo.findAll();
+    public List<Restaurant> getAllRestaurants() {
+        return restaurantService.getAllRestaurants();
     }
 
     @GetMapping(value = "/{id}")
-    public Restaurant getSingleRestaurant(@PathVariable("id") long id){
-        return restaurantRepo.findById(id).get();
+    public Restaurant getSingleRestaurant(@PathVariable("id") long id) {
+        return restaurantService.getSingleRestaurant(id).orElse(null);
     }
 
     @DeleteMapping(value = "/{id}")
-    public boolean removeRestaurant(@PathVariable("id") long id){
-        if(!restaurantRepo.findById(id).equals(Optional.empty())){
-            restaurantRepo.deleteById(id);
-            return true;
-        }
-        return false;
+    public boolean removeRestaurant(@PathVariable("id") long id) {
+        return restaurantService.removeRestaurant(id);
     }
 
     @PutMapping(value = "/{id}")
-    public Restaurant updateRestaurant(@PathVariable("id") long id, @RequestBody Map<String, Object> body){
-        Restaurant current= restaurantRepo.findById(id).get();
-        current.setName((String) body.get("name"));
-        current.setAddress((String) body.get("address"));
-        current.setTables((Set<RestaurantTable>) body.get("tables"));
-        restaurantRepo.save(current);
-        return current;
+    public Restaurant updateRestaurant(@PathVariable("id") long id, @RequestBody Map<String, Object> body) {
+        return restaurantService.updateRestaurant(id, body);
     }
 
     @PostMapping()
     public Restaurant addRestaurant(@RequestBody Map<String, Object> body) {
-        String name = (String) body.get("name");
-        String address = (String) body.get("address");
-
-        List<Map<String, Object>> tablesData = (List<Map<String, Object>>) body.get("tables");
-
-        Restaurant newRestaurant = new Restaurant();
-        newRestaurant.setName(name);
-        newRestaurant.setAddress(address);
-
-        Set<RestaurantTable> restaurantTables = new HashSet<>();
-        for (Map<String, Object> tableData : tablesData) {
-            String tableName = (String) tableData.get("name");
-            boolean available = (Boolean) tableData.get("available");
-
-            RestaurantTable restaurantTable = new RestaurantTable();
-            restaurantTable.setName(tableName);
-            restaurantTable.setAvailable(available);
-            restaurantTable.setRestaurant(newRestaurant);
-
-            restaurantTables.add(restaurantTable);
-        }
-        newRestaurant.setTables(restaurantTables);
-
-        return restaurantRepo.save(newRestaurant);
+        return restaurantService.addRestaurant(body);
     }
 }
