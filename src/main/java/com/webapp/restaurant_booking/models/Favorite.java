81834dfa36516cf0b03ac42065df9db479d04b4f
Salih <mustafahakan9215@gmail.com>
2024-12-20 16:21:14 +0300

    added restaurantName column for favorites

--- src/main/java/com/webapp/restaurant_booking/models/Favorite.java
+++ src/main/java/com/webapp/restaurant_booking/models/Favorite.java
@@ -19,11 +19,23 @@ public class Favorite {
     @JsonManagedReference
     private Restaurant restaurant;
 
+    @Column
+    private String restaurantName;
+
     public Favorite() {}
 
-    public Favorite(User user, Restaurant restaurant) {
-        this.user= user;
-        this.restaurant= restaurant;
+    public Favorite(User user, Restaurant restaurant, String restaurantName) {
+        this.user = user;
+        this.restaurant = restaurant;
+        this.restaurantName = restaurantName;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
     }
 
     public User getUser() {
